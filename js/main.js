c9a3c8d0a703a7ac5eafd7425627d3d721ab589c
Mustafa Hesham <66361736+mustafa3370@users.noreply.github.com>
2022-12-14 03:12:43 +0200

    Add files via upload
    Signed-off-by: Mustafa Hesham <66361736+mustafa3370@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-function toggle(){
-    var trailer= document.querySelector(".trailer")
-    var video= document.querySelector("video")
-    trailer.classList.toggle("active");
-    video.pause();
-    video.currentTime = 0;
+var elements = document.getElementsByClassName(".navbar");
+for(var i = 0; i < elements.length; i++){
+    elements[i].onclick = function(){
+        this.classList.add("active");
+    };        
 }
